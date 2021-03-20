6c7df00be54cb331d300c7f7f9a760155925e6d8
Muhammet Catal <mcatal55@hotmail.com>
2021-03-20 22:57:27 +0100

    first commit

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express();
+const cors = require("cors");
+const helmet = require("helmet");
+
+router.use(express.json());
+router.use(cors());
+router.use(helmet());
+
+const postRouter = require("../data/routers/post-router");
+// const userRouter = require("../data/routers/user-router");
+
+router.use("/posts", postRouter);
+// router.use("/", userRouter);
+
+router.get("/", (req, res) => {
+  res.status(200).json({ server: "up" });
+});
+
+module.exports = router;
