84d7cfa944a81321b3ef8fb21ba2aca3230cce71
Muhammet Catal <mcatal55@hotmail.com>
2021-05-08 03:10:30 +0200

    new Changes

--- api/server.js
+++ api/server.js
@@ -9,11 +9,14 @@ router.use(helmet());
 
 const postRouter = require("../data/routers/post-router");
 const productRouter = require("../data/routers/product-router");
+const orderRouter = require("../data/routers/order-router");
 // const userRouter = require("../data/routers/user-router");
 
 router.use("/posts", postRouter);
 // router.use("/", userRouter);
 router.use("/product", productRouter);
+
+router.use("/order", orderRouter);
 
 router.get("/", (req, res) => {
   res.status(200).json({ server: "up" });
